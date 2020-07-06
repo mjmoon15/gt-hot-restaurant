19cc5b472859404aad0d9c5343617158b28142b2
Mark Mooney <mjmoon15@gmail.com>
2020-07-06 10:09:53 -0400

    posting chnages from last week

--- server.js
+++ server.js
@@ -1,98 +1,101 @@
- // Dependencies
-      // =============================================================
-      var express = require("express");
-      var path = require("path");
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
 
-      // Sets up the Express App
-      // =============================================================
-      var app = express();
-      var PORT = process.env.PORT || 3000;
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
 
-      // Sets up the Express app to handle data parsing
-      app.use(express.urlencoded({ extended: true }));
-      app.use(express.json());
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-      
-      // =============================================================
-      var tables = [
-        {
-          routeName: "reservation",
-          name: "Yoda",
-          groupSize: 6,
-          
-        },
-        {
-          routeName: "reservation",
-          name: "Darth Maul",
-          groupSize: 3,
-          
-        },
-      
-      ];
-      var reserve = [
-          {
-              routeName: "waitlist",
-              name: "steve",
-              groupSize: 5,
-          },
-          {
-              routeName: "waitlist",
-              name: "tim",
-              groupSize: 10
-          }
-      ]
+// =============================================================
+var tables = [
+  {
+    routeName: "reservation",
+    name: "Yoda",
+    groupSize: 6,
+  },
+  {
+    routeName: "reservation",
+    name: "Darth Maul",
+    groupSize: 3,
+  },
+];
+var reserve = [
+  {
+    routeName: "waitlist",
+    name: "steve",
+    groupSize: 5,
+  },
+  {
+    routeName: "waitlist",
+    name: "tim",
+    groupSize: 10,
+  },
+];
 
-      // Routes
-      // =============================================================
+// Routes
+// =============================================================
 
-      // Basic route that sends the user first to the AJAX Page
-      
-      app.get("/home", function(req, res) {
-        res.sendFile(path.join(__dirname, "home.html"));
-      });
-      app.get("/tables", function(req, res) {
-        res.sendFile(path.join(__dirname, "tables.html"));
-      });
-      app.get("/reserve", function(req, res) {
-        res.sendFile(path.join(__dirname, "reserve.html"));
-      });
+// Basic route that sends the user first to the AJAX Page
 
+app.get("/home", function (req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+app.get("/tables", function (req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
-   
-      //API calls for reserve and table objects
-      app.get("/api/tables", function (req, res) {
-        return res.json(tables);
-      });
+//API calls for reserve and table objects
+app.get("/api/tables", function (req, res) {
+  return res.json(tables);
+});
+app.get("/api/reserve", function (req, res) {
+  return res.json(reserve);
+});
 
-      app.get("/api/reserve", function (req, res) {
-        return res.json(reserve);
-      });
+app.post("/api/reserve", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.routeName = newReservation.name
+      .replace(/\s+/g, "")
+      .toLowerCase();
+    console.log(newReservation);
+    characters.push(newReservation);
+    res.json(newReservation);
+  });
 
-     
-     
+// // Create New Reservations - takes in JSON input
+// app.post("/api/reserve", function (req, res) {
+//   // req.body hosts is equal to the JSON post sent from the user
+//   // This works because of our body parsing middleware
+//   var newCharacter = req.body;
 
-      // // Create New Characters - takes in JSON input
-      // app.post("/api/characters", function (req, res) {
-      //   // req.body hosts is equal to the JSON post sent from the user
-      //   // This works because of our body parsing middleware
-      //   var newCharacter = req.body;
+//   // Using a RegEx Pattern to remove spaces from newCharacter
+//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//   newCharacter.routeName = newCharacter.name
+//     .replace(/\s+/g, "")
+//     .toLowerCase();
 
-      //   // Using a RegEx Pattern to remove spaces from newCharacter
-      //   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-      //   newCharacter.routeName = newCharacter.name
-      //     .replace(/\s+/g, "")
-      //     .toLowerCase();
+//   console.log(newCharacter);
 
-      //   console.log(newCharacter);
+//   characters.push(newCharacter);
 
-      //   characters.push(newCharacter);
+//   res.json(newCharacter);
+// });
 
-      //   res.json(newCharacter);
-      // });
-
-      // Starts the server to begin listening
-      // =============================================================
-      app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-      });
-
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
